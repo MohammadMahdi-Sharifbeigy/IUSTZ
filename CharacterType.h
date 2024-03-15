9a6f72328705546c98ed8d85aeb1cad5d4d0f195
MohammadMahdi-Sharifbeigy <154359434+MohammadMahdi-Sharifbeigy@users.noreply.github.com>
2024-03-15 15:11:18 +0330

    add enum charactertype

---
+++ CharacterType.h
@@ -0,0 +1,16 @@
+#ifndef CHARACTER_TYPE_H
+#define CHARACTER_TYPE_H
+
+enum characterType
+{
+    PALADIN,
+    SUPERHERO,
+    ASSASSIN,
+    CYBORG,
+    SNIPER,
+    WIZARD,
+    WEAKZOMBIE,
+    STRONGZOMBIE
+};
+
+#endif /* CHARACTER_TYPE_H */
