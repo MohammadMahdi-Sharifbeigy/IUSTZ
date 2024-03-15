987e12f0d7b94058d047177e008e8278b4cc41e7
MohammadMahdi-Sharifbeigy <154359434+MohammadMahdi-Sharifbeigy@users.noreply.github.com>
2024-03-15 15:26:27 +0330

    change header file and fix c++ file

--- AsianMom.h
+++ AsianMom.h
@@ -1,28 +1,23 @@
-//
-//  AsianMom.h
-//  
-//
-//  Created by Lilia Rouhi on 12/21/1402 AP.
-//
+#ifndef ASIANMOM_H
+#define ASIANMOM_H
 
-#ifndef AsianMom_h
-#define AsianMom_h
-
+#include "AsianmomAttack.h"
+#include "Character.h"
+#include "Human.h"
+#include "CharacterType.h"
 
 class AsianMom : public Human
 {
-public :
-    AsianMom(string name , int age) ;
+private:
+  AttackStrategy *attackStrategy;
 
-    virtual int attack_func (Human * currEnemy , Item* item) override;
-    
-    virtual int attack_func (Character * currEnemy , Item* item) override ;
-    
-    virtual void takeDamage(int damage) override;
-    
-    virtual void levelUp() override ;
+public:
+  AsianMom(const std::string &name, double hp, double attack, double defense, characterType role);
+  ~AsianMom();
+
+  void performAttack(Character &target) override;
+  void performDefense() override {};
+  virtual void levelUp() override; 
 };
 
-
-
-#endif /* AsianMom_h */
+#endif // ASIANMOM_H
