fd0b7f8d67b8b83ae71e583ace1eae3d6bba2750
MohammadMahdi-Sharifbeigy <154359434+MohammadMahdi-Sharifbeigy@users.noreply.github.com>
2024-03-25 18:51:00 +0330

    add the other items function to humanenemy

--- HumanEnemy.h
+++ HumanEnemy.h
@@ -8,7 +8,7 @@
 #include "Human.h"
 
 class HumanEnemy : public Enemy {
- public:
+public:
   HumanEnemy(Human& humanRef);
 
   HumanEnemy(int level, Human& humanRef);
@@ -30,13 +30,23 @@ class HumanEnemy : public Enemy {
   // Items Functions
   void showInventory() { humanRef.showInventory(); }
   void addInventory(Item* item) { humanRef.addInventory(item); }
+  void addInventory(Item* item, int count){humanRef.addInventory(item, count); }
+  void removeInventory(int index, int count) {humanRef.removeInventory(index, count); }
   void removeInventory(int index) { humanRef.removeInventory(index); }
   int inventorySize() { return humanRef.inventorySize(); }
+  bool existInInventory(Item* item){return humanRef.existInInventory(item); }
+  int countInInventory(Item* item){return humanRef.countInInventory(item); }
+  int indexInInventory(Item* item){humanRef.indexInInventory(item); }
+  double priceItemsAt(int index){humanRef.priceItemsAt(index); }
+  int countItemsAt(int index){humanRef.countItemsAt(index); }
   Item* itemsAt(int index) { return humanRef.itemsAt(index); }
   Item* getArmor() { return humanRef.getArmor(); }
   Item* getWeapon() { return humanRef.getWeapon(); }
+  void setArmor(Item* armor){return humanRef.setArmor(armor); }
+  void setWeapon(Item* weapon){return humanRef.setWeapon(weapon); }
+  Item* stringToItem(const string& line){return humanRef.stringToItem(line); }
 
- private:
+private:
   Human& humanRef;
   AttackStrategy* attackStrategy;
 };
