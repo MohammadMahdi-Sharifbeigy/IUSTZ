5cca10834bd2b3dd016555b0b911b3bb7175bd7f
yousof shahrabi <yousof.shahrabi84@gmail.com>
2024-03-29 17:57:31 +0330

    remove unnecessary comments and syntax issue

--- RecommendCharacter.h
+++ RecommendCharacter.h
@@ -1,28 +1,17 @@
-//
-//  RecommendCharacter.h
-//  
-//
-//  Created by Lilia Rouhi on 1/10/1403 AP.
-//
-
 #ifndef RecommendCharacter_h
 #define RecommendCharacter_h
 
-
-struct question{
-    string question;
-    string option1;
-    string option2;
-    string option3;
-    string option4;
-    string option5;
-    string option6;
-    string option7;
+struct question {
+  string question;
+  string option1;
+  string option2;
+  string option3;
+  string option4;
+  string option5;
+  string option6;
+  string option7;
 };
 
 void recommendCharacter();
-
-
-
 
 #endif /* RecommendCharacter_h */
