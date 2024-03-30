456a5fc176ebe36772acfbfdaa558818b56c34f4
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-03-30 16:46:08 +0330

    Update Katana.cpp

--- Katana.cpp
+++ Katana.cpp
@@ -9,4 +9,4 @@ Katana::Katana(string name,
                string type,
                double synergyDamage,
                int count)
-    : Passive(name, price, vIP, perk, type, synergyDamage, count){};
+    : Passive(name, price, vIP, perk, type, synergyDamage, count), ID(10){};
