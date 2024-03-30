7bce08ccfde3bdd1982d7e737cb6b7e211d2db49
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-03-30 17:25:56 +0330

    Update LaserGun.cpp

--- LaserGun.cpp
+++ LaserGun.cpp
@@ -9,4 +9,4 @@ LaserGun::LaserGun(string name,
                    string type,
                    double synergyDamage,
                    int count)
-    : Passive(name, price, vIP, perk, type, synergyDamage, count), ID(14){};
+    : Passive(name, price, vIP, perk, type, synergyDamage, count){this->ID=14;};
