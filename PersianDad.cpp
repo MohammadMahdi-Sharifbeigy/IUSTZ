a2bd7e32feb8c2a2c96483b98323f9c0fa309ba3
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-03-30 16:44:00 +0330

    Update PersianDad.cpp

--- PersianDad.cpp
+++ PersianDad.cpp
@@ -10,4 +10,4 @@ PersianDad::PersianDad(string name,
                        string perk,
                        string type,
                        double synergyDamage, int count)
-    : Passive(name, price, vIP, perk, type, synergyDamage, count) {}
+    : Passive(name, price, vIP, perk, type, synergyDamage, count) , ID(7) {}
