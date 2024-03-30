afc24b3e0cc45e620cebf046a8289bc24ff27f29
Mahsa Soleimani <154425249+Mahsesol@users.noreply.github.com>
2024-03-31 03:17:07 +0330

    Create ACSIIArt.h
    uploading ASCII arts .h file

---
+++ ASCIIArt/ACSIIArt.h
@@ -0,0 +1,22 @@
+#ifndef ASCIIART_h
+#define ASCIIART_h
+#include <iostream>
+
+using namespace std;
+
+const string Wellcome1;
+const string Wellcome2;
+const string Superhero1;
+const string Superhero2;
+const string Paladin1;
+const string Paladin2;
+const string Assassin;
+const string Cyborg;
+const string Wizard;
+const string AsianMom;
+const string Sniper;
+const string DeathAngel;
+const string Zambia1;
+
+
+#endif  // * ASCIIART_H
