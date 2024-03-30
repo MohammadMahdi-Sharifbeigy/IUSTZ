b30c10c37d7205a08ce0eb9d373939060c715140
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-03-30 16:48:52 +0330

    Update LittleRobots.cpp

--- LittleRobots.cpp
+++ LittleRobots.cpp
@@ -9,4 +9,4 @@ LittleRobots::LittleRobots(string name,
                            string type,
                            double synergyDamage,
                            int count)
-    : Passive(name, price, vIP, perk, type, synergyDamage, count){};
+    : Passive(name, price, vIP, perk, type, synergyDamage, count) , ID(15){};
