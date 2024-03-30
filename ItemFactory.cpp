5694ee0c13471c1ee7bb46ac4b615907250621f3
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-03-30 15:35:47 +0330

    Update ItemFactory.cpp constructor

--- ItemFactory.cpp
+++ ItemFactory.cpp
@@ -3,6 +3,9 @@
 #include <string>
 using namespace std;
 
+
+
+ItemFactory() =  default;
 Item* ItemFactory::createItem(int choice, Human* human, bool login) {
   Item* item = nullptr;
   string name, perk = "Default Perk", type = "Default Type";
