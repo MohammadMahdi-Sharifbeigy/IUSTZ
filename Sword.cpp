c4c6162ffded5be04ba53e66fc5d209631ed0b7c
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-03-30 16:45:17 +0330

    Update Sword.cpp

--- Sword.cpp
+++ Sword.cpp
@@ -9,4 +9,4 @@ Sword::Sword(string name,
              string type,
              double synergyDamage,
              int count)
-    : Passive(name, price, vIP, perk, type, synergyDamage, count){};
+    : Passive(name, price, vIP, perk, type, synergyDamage, count), ID(8){};
