088fbaee11ae8f6ae3962e475c9e665da258fd12
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-03-30 17:33:29 +0330

    Update Flip_flops.cpp

--- Flip_flops.cpp
+++ Flip_flops.cpp
@@ -7,4 +7,4 @@ Flipflops::Flipflops(string name,
                      bool vIP,
                      double synergyDamage,
                      int count)
-    : Throwable(name, price, vIP, synergyDamage, count), ID(28){};
+    : Throwable(name, price, vIP, synergyDamage, count){this->ID=28;};
