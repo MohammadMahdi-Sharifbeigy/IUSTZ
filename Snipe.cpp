1fc227cdba045d45cb19ad725e3f16cd97674cf6
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-03-30 17:27:01 +0330

    Update Snipe.cpp

--- Snipe.cpp
+++ Snipe.cpp
@@ -9,4 +9,4 @@ Snipe::Snipe(string name,
              string type,
              double synergyDamage,
              int count)
-    : Passive(name, price, vIP, perk, type, synergyDamage, count), ID(16){};
+    : Passive(name, price, vIP, perk, type, synergyDamage, count){this->ID=16;};
