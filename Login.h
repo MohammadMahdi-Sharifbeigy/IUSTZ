ce53d8df24700480093c288ba4aa0df73a2f4319
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-03-30 18:25:18 +0330

    Add files via upload

---
+++ Login.h
@@ -0,0 +1,33 @@
+//
+//  RecommendCharacter.h
+//  
+//
+//  Created by Lilia Rouhi on 1/10/1403 AP.
+//
+
+#ifndef RecommendCharacter_h
+#define RecommendCharacter_h
+#include <iostream>
+#include <string.h>
+#include "Human.h"
+
+
+using namespace std;
+
+struct question{
+    string questionText;
+    string option1;
+    string option2;
+    string option3;
+    string option4;
+    string option5;
+    string option6;
+    string option7;
+};
+
+Human* Login();
+
+
+
+
+#endif /* RecommendCharacter_h */
