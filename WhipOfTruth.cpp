7c74d231c6100d66dce91ad71e28ea6e33eb12bb
liliarouhi <154587589+liliarouhi@users.noreply.github.com>
2024-03-30 17:28:19 +0330

    Update WhipOfTruth.cpp

--- WhipOfTruth.cpp
+++ WhipOfTruth.cpp
@@ -9,4 +9,4 @@ WhipOfTruth::WhipOfTruth(string name,
                          string type,
                          double synergyDamage,
                          int count)
-    : Passive(name, price, vIP, perk, type, synergyDamage, count), ID(19){};
+    : Passive(name, price, vIP, perk, type, synergyDamage, count){this->ID=19;};
